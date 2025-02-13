8618bb0ab06d96400238301912f49c1f6b3d5ec3
vukoebina <chris.tochka@gmail.com>
2025-02-13 17:11:39 +0100

    changed main to default

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-def index(request):
-    return render(request, "mainapp/index.html")
-
-from django.shortcuts import render
 from .tasks import dummy_and_slow # new
 import time # new
 
@@ -15,7 +7,8 @@ def index(request):
 
 def dummy_and_slow_view(request): # new
     start_time = time.time()
-    dummy_and_slow()
+    r = dummy_and_slow.delay()
+    print(r)
     end_time = time.time()
     execution_time = end_time - start_time
     context = {"task_name": "Dummy and slow", "execution_time": round(execution_time, 2)}
